a7f25550d02e5d391c80904810666bbfd44a415f
@ravimourya <ravindrakushawaha007@gmail.com>
2026-03-15 18:13:48 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "industrial-network",
+  "version": "1.0.0",
+  "description": "Hammerwold Industrial Skill Network",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "socket.io": "^4.7.2",
+    "pg": "^8.11.0"
+  }
+}
